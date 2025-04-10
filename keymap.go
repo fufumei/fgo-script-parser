dd5d75d1af6fdd0488d6afb2ca2578f308d237c1
fumei <robin.fumei@gmail.com>
2025-04-11 00:10:28 +0200

    Replace all old files with new structure

--- keymap.go
+++ keymap.go
@@ -3,63 +3,38 @@ package main
 import "github.com/charmbracelet/bubbles/key"
 
 type KeyMap struct {
-	NextState  key.Binding
-	PrevState  key.Binding
-	NextOption key.Binding
-	PrevOption key.Binding
-	Confirm    key.Binding
-	Toggle     key.Binding
-	Attach     key.Binding
-	Unattach   key.Binding
-	Back       key.Binding
-	Quit       key.Binding
+	NextState     key.Binding
+	PrevState     key.Binding
+	NextOption    key.Binding
+	PrevOption    key.Binding
+	NextSubOption key.Binding
+	PrevSubOption key.Binding
+	Toggle        key.Binding
+	BlurInput     key.Binding
+	FocusInput    key.Binding
+	Confirm       key.Binding
+	Quit          key.Binding
+
+	Copy key.Binding
+	// CopyAll key.Binding
 }
 
 func DefaultKeybinds() KeyMap {
 	return KeyMap{
-		NextState: key.NewBinding(
-			key.WithKeys("tab"),
-			key.WithHelp("tab", "next"),
-		),
-		PrevState: key.NewBinding(
-			key.WithKeys("shift+tab"),
-			key.WithHelp("shift+tab", "prev"),
-		),
-		NextOption: key.NewBinding(
-			key.WithKeys("down"),
-		),
-		PrevOption: key.NewBinding(
-			key.WithKeys("up"),
-		),
-		Toggle: key.NewBinding(
-			key.WithKeys("enter"),
-			key.WithHelp("enter", "toggle"),
-			key.WithDisabled(),
-		),
-		Confirm: key.NewBinding(
-			key.WithKeys("enter"),
-			key.WithHelp("enter", "confirm"),
-			key.WithDisabled(),
-		),
-		Attach: key.NewBinding(
-			key.WithKeys("enter"),
-			key.WithHelp("enter", "attach file"),
-			key.WithDisabled(),
-		),
-		Unattach: key.NewBinding(
-			key.WithKeys("x"),
-			key.WithHelp("x", "remove"),
-			key.WithDisabled(),
-		),
-		Back: key.NewBinding(
-			key.WithKeys("esc"),
-			key.WithHelp("esc", "back"),
-			key.WithDisabled(),
-		),
-		Quit: key.NewBinding(
-			key.WithKeys("ctrl+q"),
-			key.WithHelp("ctrl+q", "quit"),
-		),
+		NextState:     key.NewBinding(key.WithKeys("tab"), key.WithHelp("tab", "next")),
+		PrevState:     key.NewBinding(key.WithKeys("shift+tab"), key.WithHelp("shift+tab", "back")),
+		NextOption:    key.NewBinding(key.WithKeys("down"), key.WithHelp("↑/↓", "up/down")),
+		PrevOption:    key.NewBinding(key.WithKeys("up")),
+		NextSubOption: key.NewBinding(key.WithKeys("right"), key.WithHelp("←/→", "left/right"), key.WithDisabled()),
+		PrevSubOption: key.NewBinding(key.WithKeys("left")),
+		Toggle:        key.NewBinding(key.WithKeys("enter"), key.WithHelp("enter", "toggle"), key.WithDisabled()),
+		BlurInput:     key.NewBinding(key.WithKeys("esc"), key.WithHelp("esc", "stop"), key.WithDisabled()),
+		FocusInput:    key.NewBinding(key.WithKeys("enter"), key.WithHelp("enter", "type"), key.WithDisabled()),
+		Confirm:       key.NewBinding(key.WithKeys("enter"), key.WithHelp("enter", "confirm"), key.WithDisabled()),
+		Quit:          key.NewBinding(key.WithKeys("ctrl+q"), key.WithHelp("ctrl+q", "quit")),
+
+		Copy: key.NewBinding(key.WithKeys("ctrl+c"), key.WithHelp("ctrl+c", "copy row"), key.WithDisabled()),
+		// CopyAll: key.NewBinding(key.WithKeys("ctrl+a"), key.WithHelp("ctrl+a", "copy all"), key.WithDisabled()),
 	}
 }
 
@@ -68,9 +43,14 @@ func (k KeyMap) ShortHelp() []key.Binding {
 	return []key.Binding{
 		k.NextState,
 		k.PrevState,
+		k.NextOption,
+		k.Copy,
+		// k.CopyAll,
+		k.NextSubOption,
 		k.Toggle,
+		k.BlurInput,
+		k.FocusInput,
 		k.Confirm,
-		k.Attach, k.Unattach,
 		k.Quit,
 	}
 }
@@ -78,28 +58,35 @@ func (k KeyMap) ShortHelp() []key.Binding {
 // FullHelp returns the key bindings for the full help screen.
 func (k KeyMap) FullHelp() [][]key.Binding {
 	return [][]key.Binding{
-		{k.NextState, k.Confirm, k.Attach, k.Unattach, k.Quit},
+		{},
 	}
 }
 
 func (m *Model) updateKeymap() {
-	m.keymap.NextState.SetEnabled(m.state != ConfirmButton)
-	m.keymap.PrevState.SetEnabled(m.state != SourceSelect)
-	m.keymap.NextOption.SetEnabled(m.state == SourceSelect || m.state == AtlasTypeSelect)
-	m.keymap.PrevOption.SetEnabled(m.state == SourceSelect || m.state == AtlasTypeSelect)
-	m.keymap.Confirm.SetEnabled(m.state == ConfirmButton)
-	m.keymap.Toggle.SetEnabled(m.state == MiscOptions)
-	m.keymap.Back.SetEnabled(m.state == PickingFile)
-	m.keymap.Attach.SetEnabled(m.state == IdInput && m.source == local)
-	m.keymap.Unattach.SetEnabled(m.state == IdInput && m.source == local && len(m.Attachments.Items()) > 0)
+	stateHasOptions := m.currentState == SourceSelect || m.currentState == AtlasTypeSelect
+	hasNextstate := true
 
-	m.filepicker.KeyMap.Up.SetEnabled(m.state == PickingFile && m.source == local)
-	m.filepicker.KeyMap.Down.SetEnabled(m.state == PickingFile && m.source == local)
-	m.filepicker.KeyMap.Back.SetEnabled(m.state == PickingFile && m.source == local)
-	m.filepicker.KeyMap.Select.SetEnabled(m.state == PickingFile && m.source == local)
-	m.filepicker.KeyMap.Open.SetEnabled(m.state == PickingFile && m.source == local)
-	m.filepicker.KeyMap.PageUp.SetEnabled(m.state == PickingFile && m.source == local)
-	m.filepicker.KeyMap.PageDown.SetEnabled(m.state == PickingFile && m.source == local)
-	m.filepicker.KeyMap.GoToTop.SetEnabled(m.state == PickingFile && m.source == local)
-	m.filepicker.KeyMap.GoToLast.SetEnabled(m.state == PickingFile && m.source == local)
+	if m.currentState == Results {
+		hasNextstate = false
+	} else if m.currentState == Confirm {
+		if len(m.results) > 0 {
+			hasNextstate = true
+		} else {
+			hasNextstate = false
+		}
+	} else {
+		hasNextstate = true
+	}
+
+	m.keymap.NextState.SetEnabled(hasNextstate)
+	m.keymap.PrevState.SetEnabled(m.currentState != SourceSelect)
+	m.keymap.NextOption.SetEnabled(stateHasOptions || m.resultsTable.Focused())
+	m.keymap.PrevOption.SetEnabled(stateHasOptions)
+	m.keymap.Toggle.SetEnabled(m.currentState == MiscOptions)
+	m.keymap.Confirm.SetEnabled(m.currentState == Confirm)
+	m.keymap.BlurInput.SetEnabled(m.currentState == IdInput && m.IdInput.Focused())
+	m.keymap.FocusInput.SetEnabled(m.currentState == IdInput && !m.IdInput.Focused())
+
+	m.keymap.Copy.SetEnabled(m.resultsTable.Focused())
+	// m.keymap.CopyAll.SetEnabled(m.resultsTable.Focused())
 }
